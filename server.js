d684b9f20c895e72eda14ca4692b29da9dfadb9b
Shane Orend <wagen00@gmail.com>
2017-12-05 14:23:27 -0500

    html update

---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var exphbs  = require('express-handlebars');
